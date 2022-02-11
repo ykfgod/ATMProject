4607b5a601d0f42b3a314b01dcab524abb79c615
ykf <ykfgodnew@gmail.com>
2022-02-11 11:46:34 +0300

    Создание переменных класа Transaction

--- src/com/company/Transaction.java
+++ src/com/company/Transaction.java
@@ -1,4 +1,21 @@
 package com.company;
+import  java.util.Date;
 
 public class Transaction {
+    /**
+     * Переменая содержащая сумму этой транзакции
+     */
+    private  double amount;
+    /**
+     * Переменая содержащая дату и время этой транзакции
+     */
+    private  Date timestamp;
+    /**
+     * Переменая содержащая описание этой транзакции
+     */
+    private  String memo;
+    /**
+     * Переменая содержащая имя аккаунта на которм совершена транзакция
+     */
+    private  Account inAccount;
 }
