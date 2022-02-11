980934ea5a6a97c67b8485fac9f63b4328b1163c
ykf <ykfgodnew@gmail.com>
2022-02-11 11:39:22 +0300

    Создание переменных класа Account

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -1,4 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Account {
+    /**
+     * Переменая содержащая имя аккаунта
+     */
+    private  String name;
+    /**
+     * Переменая содержащая баланс аккаунта
+     */
+    private  double balance;
+    /**
+     * Переменая содержащая ID номер аккаунта
+     */
+    private  String uuid;
+    /**
+     * Переменая содержащая объект User владельца этого аккаунта
+     */
+    private  User holder;
+    /**
+     * Переменая содержащая список транзакций этого аккаунта
+     */
+    private ArrayList<Transaction> transactions;
 }
