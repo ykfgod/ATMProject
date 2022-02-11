e42cd2ab3fc755148b81d0893b35da92e1186a80
ykf <ykfgodnew@gmail.com>
2022-02-11 11:52:23 +0300

    Создание переменных класа Bank

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Bank {
+    /**
+     * Переменая содержащая название банка
+     */
+    private String name;
+    /**
+     * Переменая содержащая список объектов пользвателей банка
+     */
+    private ArrayList<User> user;
+    /**
+     * Переменая содержащая список объектов аккаунтов банка
+     */
+    private  ArrayList<Account> accounts;
 }
